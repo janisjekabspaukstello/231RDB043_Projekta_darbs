03240850fb40f6a10a4219439e879ca32f17e2f6
janisjekabspaukstello <144729163+janisjekabspaukstello@users.noreply.github.com>
2024-01-18 22:39:33 +0000

    00:39

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # 231RDB043_Projekta_darbs
-### ReadMe punkti:
-    · detalizēti aprakstīsiet projekta uzdevumu,
-    · izskaidrosiet kādas Python bibliotēkas un kāpēc tiek izmantotas projekta izstrādes laikā,
-    · aprakstīsies programmatūras izmantošanas metodes;
 ## Projekta uzdevums
-    .
+    Projekta uzdevums ir izveidot programmu. kas sarakstā, kas atrodas excel failā pēc dotās informācijas atgriež tajā pašā failā saites uz jaunākajiem video Youtube kanālos no sarakstā norādītajiem.
 
 ## Bibliotēkas
-    .
+    Šinī projektā izmantotas selenium un openpyxl bibliotēkas. Selenium bibliotēka izmantota lai piekļūtu interneta resursiem, lai varētu izgūt informāciju par jaunākajiem videomateriāliem atlsītajos kanālos.
 
 ## Programmatūras metodes
-    .
+    Programmatūru var izmantot pielāgojot excel sarakstu ar youtube kanālu video saraksta lapām un palaižot programmu iegūstot jaunāko informāciju par visu kanālu jaunākajiem video.
--- project.py
+++ project.py
@@ -11,32 +11,8 @@ driver = webdriver.Chrome(service=service, options=option)
 
 dati=load_workbook('dati.xlsx')
 speletaji=dati['Latvijas_Speletaji']
-speles=dati['Nesenas_Speles']
 kanali=dati['YT_kanali']
 
-lastRow = speletaji.max_row
-for rinda in range(2,lastRow+1):
-    vards=(speletaji['a' + str(rinda)].value) + " " + (speletaji['b' + str(rinda)].value)
-    liga=(speletaji['c' + str(rinda)].value)
-    komanda=(speletaji['d' + str(rinda)].value)
-    if(liga == "NBA"):
-        url = "https://www.nba.com/games"
-        driver.get(url)
-        time.sleep(2)
-        # nba get
-        # decline coocies
-    if(liga == "EuroCup"):
-        url = "https://www.euroleaguebasketball.net/eurocup/game-center/"
-        driver.get(url)
-        time.sleep(2)
-        # eurocup get
-        # accept coocies
-    if(liga == "EuroLeague"):
-        url = "https://www.euroleaguebasketball.net/euroleague/game-center/"
-        driver.get(url)
-        time.sleep(2)
-        # euroleague get
-        # accept coocies
 lastRow = kanali.max_row
 for rinda in range(2,lastRow+1):
     saite=(kanali['d' + str(rinda)].value)
@@ -44,10 +20,15 @@ for rinda in range(2,lastRow+1):
     url = str(saite)
     driver.get(url)
     time.sleep(2)
-    #pedejais video
-    jaunais = 0
-    #
+    cookies_no = driver.find_element(By.CLASS_NAME,"VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc iMLaPd")
+    cookies_no.click
+    time.sleep(2)
+    latest = driver.find_element(By.ID,"video-title-link")
+    saite = latest.get_attribute("href")
+    jaunais = saite
     if(pedejais == jaunais):
         kanali['c'+str(rinda)].value= "tas pats"
     else:
         kanali['c'+str(rinda)].value= "JAUNS"
+        kanali['b'+str(rinda)].value= jaunais
+###
